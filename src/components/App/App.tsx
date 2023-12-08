f6ff670a7e6d06b444c5d6c1c2f4c6b2164d5bbe
Ilia Lebedev <ilia@neon.tech>
2023-12-09 00:39:49 +0100

    code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,12 +4,108 @@ import { ControlShape, init } from './audio.ts';
 import audioTrack from './outfoxing.mp3';
 import styles from './App.module.css';
 
+const N = 100;
+
+function drawWave(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+  buffer: Uint8Array,
+  n: number,
+  secondColor: boolean,
+  offsetRatio: number,
+): void {
+  const width = canvas.width / N;
+  let x = n * width;
+
+  ctx.fillStyle = secondColor ? 'rgb(200, 200, 200)' : 'rgb(230, 230, 230)';
+  ctx.fillRect(x, 0, width, canvas.height);
+
+  ctx.beginPath();
+  ctx.moveTo(x, canvas.height / 2);
+  ctx.lineTo(x, canvas.height);
+  ctx.strokeStyle = 'rgba(255,0,0,0.1)';
+  ctx.stroke();
+
+  ctx.beginPath();
+
+  const bufferStart = Math.floor(offsetRatio * buffer.length);
+  // const bufferStart = 0;
+  const sliceWidth = width / (buffer.length - bufferStart - 1);
+
+  for (let i = bufferStart; i < buffer.length; i += 1) {
+    const v = buffer[i] / 128.0;
+    const y = (v * canvas.height) / 2;
+
+    if (i === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      ctx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'rgb(0, 0, 0)';
+  ctx.stroke();
+}
+
+function drawFreq(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+  buffer: Uint8Array,
+): void {
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const barWidth = (canvas.width - buffer.length) / buffer.length;
+  let x = 0;
+
+  for (let i = 0; i < buffer.length; i++) {
+    const barHeight = (buffer[i] / 255 - 0.5) * 2; // [-1, 1]
+
+    ctx.fillStyle = `rgb(${100 + Math.abs(barHeight) * 128}, 50, 50)`;
+    // ctx.fillRect(x, canvas.height, barWidth, -barHeight * canvas.height);
+    ctx.fillRect(
+      x,
+      0.5 * canvas.height,
+      barWidth,
+      -barHeight * (0.95 * 0.5 * canvas.height),
+    );
+
+    x += barWidth + 1;
+
+    // const barHeight = buffer[i] / 2;
+    //
+    // ctx.fillStyle = `rgb(${barHeight + 100}, 50, 50)`;
+    // ctx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight);
+    //
+    // x += barWidth + 1;
+  }
+}
+
+function drawXAxis(
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D,
+): void {
+  ctx.beginPath();
+  ctx.strokeStyle = 'rgb(30,30,30)';
+  ctx.lineWidth = 1;
+  ctx.moveTo(0, canvas.height / 2);
+  ctx.lineTo(canvas.width, canvas.height / 2);
+  ctx.stroke();
+}
+
 export function App() {
   const audioRef = useRef<HTMLAudioElement>(null);
   const controlsRef = useRef<ControlShape>();
   const initRef = useRef(false);
   const [volume, setVolume] = useState(1);
   const [balance, setBalance] = useState(0);
+  const [offsetRatio, setOffsetRatio] = useState(0);
+  const offsetRatioRef = useRef(offsetRatio);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvas2Ref = useRef<HTMLCanvasElement>(null);
 
   useEffect(
     () => () => {
@@ -18,6 +114,71 @@ export function App() {
     },
     [],
   );
+
+  const [playState, setPlayState] = useState(false);
+
+  useEffect(() => {
+    if (controlsRef.current) {
+      void controlsRef.current.toggle(playState);
+    }
+  }, [playState]);
+
+  useEffect(() => {
+    if (!playState) {
+      return;
+    }
+
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+
+    const canvas2 = canvas2Ref.current!;
+    const ctx2 = canvas2.getContext('2d')!;
+
+    let lastRequestAnimationFrameId: number | undefined;
+
+    let num = 0;
+    let secondColor = false;
+
+    function draw() {
+      num += 1;
+
+      if (num === N) {
+        // ctx.fillStyle = 'rgb(200, 200, 200)';
+        // ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        num = 0;
+        secondColor = !secondColor;
+      }
+
+      const ctl = controlsRef.current!;
+
+      ctl.updateAnalyserData();
+      drawWave(
+        canvas,
+        ctx,
+        ctl.analyserBuffer,
+        num,
+        secondColor,
+        offsetRatioRef.current,
+      );
+
+      drawFreq(canvas2, ctx2, ctl.freqAnalyserBuffer);
+
+      drawXAxis(canvas, ctx);
+
+      ctx.beginPath();
+
+      lastRequestAnimationFrameId = requestAnimationFrame(draw);
+    }
+
+    draw();
+
+    return () => {
+      if (lastRequestAnimationFrameId) {
+        cancelAnimationFrame(lastRequestAnimationFrameId);
+      }
+    };
+  }, [playState]);
 
   return (
     <div>
@@ -68,23 +229,56 @@ export function App() {
             }}
           />
         </label>
+
+        <label>
+          Offset ratio:
+          <input
+            type="range"
+            id="volume"
+            min="0"
+            max="1"
+            value={offsetRatio}
+            step="0.01"
+            onChange={(event) => {
+              const newValue = Number(event.target.value);
+              offsetRatioRef.current = newValue;
+              setOffsetRatio(newValue);
+            }}
+          />
+          <span>{offsetRatio}</span>
+        </label>
+
         <button
           type="button"
           data-playing="false"
           role="switch"
           aria-checked="false"
-          onClick={async () => {
+          onClick={() => {
             if (!initRef.current) {
               initRef.current = true;
               controlsRef.current = init(audioRef.current!);
+              setPlayState(true);
             }
 
-            await controlsRef.current!.play();
+            setPlayState(!playState);
           }}
         >
           <span>Play/Pause</span>
         </button>
       </div>
+
+      <canvas
+        ref={canvasRef}
+        width={1200}
+        height={300}
+        style={{ border: '1px solid #000' }}
+      />
+      <canvas
+        ref={canvas2Ref}
+        width={1200}
+        height={300}
+        style={{ border: '1px solid #000' }}
+      />
     </div>
   );
 }
--- src/components/App/audio.ts
+++ src/components/App/audio.ts
@@ -1,11 +1,17 @@
 export type ControlShape = {
   gainParam: AudioParam;
   balanceParam: AudioParam;
+  analyserBuffer: Uint8Array;
+  freqAnalyserBuffer: Uint8Array;
   play: () => Promise<void>;
+  toggle: (isPlay: boolean) => Promise<void>;
   destroy: () => Promise<void>;
+  updateAnalyserData: () => void;
 };
 
 export function init(audioElement: HTMLAudioElement): ControlShape {
+  let isPlaying = false;
+
   const audioContext = new AudioContext();
 
   const track = audioContext.createMediaElementSource(audioElement);
@@ -17,19 +23,60 @@ export function init(audioElement: HTMLAudioElement): ControlShape {
   const pannerOptions = { pan: 0 };
   const pannerNode = new StereoPannerNode(audioContext, pannerOptions);
 
-  track.connect(gainNode).connect(pannerNode).connect(audioContext.destination);
+  const analyser = new AnalyserNode(audioContext, {
+    // fftSize: 256,
+    fftSize: 1024,
+  });
+
+  const dataArray = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteTimeDomainData(dataArray);
+
+  const analyserFreq = new AnalyserNode(audioContext, {
+    fftSize: 256,
+    // fftSize: 1024,
+  });
+
+  const freqDataArray = new Uint8Array(analyserFreq.frequencyBinCount);
+  analyserFreq.getByteTimeDomainData(freqDataArray);
+
+  track
+    .connect(gainNode)
+    .connect(pannerNode)
+    .connect(analyser)
+    .connect(analyserFreq)
+    .connect(audioContext.destination);
 
   return {
     gainParam: gainNode.gain,
     balanceParam: pannerNode.pan,
+    analyserBuffer: dataArray,
+    freqAnalyserBuffer: freqDataArray,
     play: async () => {
       if (audioContext.state === 'suspended') {
         await audioContext.resume();
       }
-      await audioElement.play();
+      if (!isPlaying) {
+        await audioElement.play();
+      }
+    },
+    toggle: async (isPlay) => {
+      if (isPlay === isPlaying) {
+        return;
+      }
+
+      isPlaying = !isPlaying;
+      if (isPlaying) {
+        await audioElement.play();
+      } else {
+        audioElement.pause();
+      }
     },
     destroy: async () => {
       await audioContext.close();
+    },
+    updateAnalyserData: () => {
+      analyser.getByteTimeDomainData(dataArray);
+      analyserFreq.getByteTimeDomainData(freqDataArray);
     },
   };
 }
